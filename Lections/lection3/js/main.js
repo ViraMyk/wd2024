9d14dd7335781a6ab8a7a43066c93f33caaa348f
Roman Mykolaichuk <mykroman@ukr.net>
2022-10-03 18:13:01 +0300

    +

---
+++ Lections/lection3/js/main.js
@@ -0,0 +1,70 @@
+
+let b=0;
+
+function increment_b() {
+    b++;
+    document.getElementById("res1").innerText=b;
+}
+
+
+//alert, console, script + src
+
+//variables let const
+
+//string, numbers, boolean, null, undefined typeof
+
+
+// string concate
+
+// template string ` ${} `
+
+//string length split
+
+//arrays [2] push unshift Array.isArray indeOf
+
+//object . object array
+
+//JSON stringify 
+
+
+//for 
+
+
+//for array let item of items
+
+
+//forEach map filter
+
+
+// if == === else
+
+
+// ? : ;
+
+
+//switch
+
+
+// function arrow
+
+//construct object via function prototype?
+
+//class
+
+
+//dom
+
+//select single by id querySelector
+
+//multiple querySelectorAll className
+
+
+//const ul = document.qS ul. ... remove children text innerHTML
+
+//addEventListener preventDefault 
+
+// style classList toggle
+
+//form processing append list items
+
+
